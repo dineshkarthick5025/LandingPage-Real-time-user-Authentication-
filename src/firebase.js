46e6903be639eaa7690604cca715043044602168
Dinesh Karthick V <144465329+dineshkarthick5025@users.noreply.github.com>
2025-03-10 11:39:35 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
 const firebaseConfig = {
     apiKey: "AIzaSyCOLA3KaSeX_WuN137fvXZ-cZiWq9gLGh8",
     authDomain: "userauthetication-35c21.firebaseapp.com",
@@ -8,9 +9,19 @@ const firebaseConfig = {
     messagingSenderId: "707295691668",
     appId: "1:707295691668:web:b9c5285c04350e1b568581",
     measurementId: "G-0NK2YXG4T0"
-  }; 
+}; 
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
-export { auth };
+const signInWithGoogle = async () => {
+    try {
+        const result = await signInWithPopup(auth, provider);
+        console.log("User signed in:", result.user);
+    } catch (error) {
+        console.error("Error during Google Sign-In:", error);
+    }
+};
+
+export { auth, signInWithGoogle };
